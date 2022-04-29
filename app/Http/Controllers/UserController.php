5b7cf809a2f0e4322ae008e672baf9d9a57375f2
esraaas2018 <esraaas2018@gmail.com>
2022-04-30 02:54:11 +0300

    init

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserRegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(UserRegisterRequest $request){
+        $user = User::create([
+            'name'=>$request->name,
+            'phone_number'=>$request->phone_number,
+            'password'=> Hash::make($request->password)
+        ]);
+        $token = $user->createToken('token')->plainTextToken;
+        return response()->json([
+            'token' => $token
+        ],201);
+    }
+
+    public function login(UserLoginRequest $request){
+        $user = User::query()->where('phone_number' , $request->phone_number)->first();
+        if($user && Hash::check( $request->password , $user->password )){
+            $token = $user->createToken('token')->plainTextToken;
+            return response()->json([
+                'token' => $token
+            ],200);
+        }
+        else{
+            return response()->json([
+                'message' => "incorrect credentials"
+            ],401);
+        }
+
+    }
+
+    public  function logout(){
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'logout successfuly'
+        ]);
+    }
+}
